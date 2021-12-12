9872b3536e804418928fb86feb1fd39467555275
GAZON17 <95870147+GAZON17@users.noreply.github.com>
2021-12-12 04:46:16 +0300

    Add files via upload

---
+++ 1zadanie.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    double a, b, c;
+    char op;
+    cout << "Доступные операции: +,-,*,/" << endl;
+    while (true)
+    {
+        cout << "Введите пример: ";
+        cin >> a >> op >> b;
+        switch (op)
+        {
+        case '+':   
+        {
+            c = a + b;
+            cout << "Результат: " << c << endl;
+        }break;
+        case '-':  
+        {
+            c = a - b;
+            cout << "Результат: " << c << endl;
+        }break;
+        case '*':  
+        {
+            c = a * b;
+            cout << "Результат: " << c << endl;
+        }break;
+        case '/':   
+        {
+            if (b != 0)
+            {
+                c = a / b;
+                cout << "Результат: " << c << endl;
+            }
+            else cout << "На ноль делить нельзя." << endl;
+        }break;
+        default:    cout << "Неизвестная операция." << endl;
+        }
+        system("PAUSE");
+    }
+}
